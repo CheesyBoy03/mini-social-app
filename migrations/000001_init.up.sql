c1a109d30a97f7df08e48184bb670041c48c4cb5
Ivan Stepanov <polite.cheese03@gmail.com>
2023-05-09 17:08:46 +0300

    add migrate in the docker compose

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -1,7 +1,17 @@
 CREATE TABLE users (
-    "id"            SERIAL PRIMARY KEY,
-    "email"         VARCHAR(255) NOT NULL UNIQUE,
-    "password_hash" VARCHAR(255) NOT NULL,
-    "first_name"    VARCHAR(25) NOT NULL,
-    "last_name"     VARCHAR(55) NOT NULL
+    id            SERIAL PRIMARY KEY,
+    email         VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    first_name    VARCHAR(25) NOT NULL,
+    last_name     VARCHAR(55) NOT NULL
 );
+
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    author_id INT NOTE NULL,
+    title VARCHAR(70) NOT NULL,
+    description VARCHAR(255),
+    published_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (author_id),
+    FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE CASCADE,
+)
